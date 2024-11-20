2eca424ac6a604b8d804db32272272bdb28eea6f
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-11-20 17:55:31 -0600

    add: connection to server

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
+import Home from './pages/Home/Home.jsx'
+import NotFound from './pages/NotFound/NotFound.jsx'
+
+
 
 function App() {
-
-
-  return (
-    <>
-      
-    </>
-  )
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  return ( 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home BASE_URL={BASE_URL}/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
