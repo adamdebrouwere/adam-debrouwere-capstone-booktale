217faef30629a0feebf7d6ad3fd8bbc5fb874098
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-12-01 20:46:08 -0600

    final commit before submission

--- src/components/PastTalesDisplay/PastTalesDisplay.jsx
+++ src/components/PastTalesDisplay/PastTalesDisplay.jsx
@@ -1,5 +1,5 @@
 import "./PastTalesDisplay.scss";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthentication } from "../AuthenticationContext/AuthenticationContext";
 
