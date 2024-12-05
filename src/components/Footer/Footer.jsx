5c6f0099a700535ab4b216933bcd7d7a68f64a7a
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-12-05 13:48:29 -0600

    fix: comment sizing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -36,7 +36,7 @@ function Footer() {
           >
             <img
               className="footer__item-icon"
-              src="./images/icons/email.svg"
+              src="public/images/icons/email.svg"
               alt="email icon"
             />
           </a>
@@ -57,7 +57,7 @@ function Footer() {
           >
             <img
               className="footer__item-icon"
-              src="./images/icons/linkedin.svg"
+              src="public/images/icons/linkedin.svg"
               alt="linkedIn icon"
             />
           </a>
@@ -80,7 +80,7 @@ function Footer() {
           >
             <img
               className="footer__item-icon"
-              src="./images/icons/git.svg"
+              src="public/images/icons/git.svg"
               alt="git icon"
             />
           </a>
